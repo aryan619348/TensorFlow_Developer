55531594e9549c60b7303ae672ea0ed17af92f0b
aryan619348 <as2pilla@uwaterloo.ca>
2021-11-01 19:08:17 +0700

    trial

--- COURSE1/hello_worldDL.py
+++ COURSE1/hello_worldDL.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
-
+# FIRST PROGRAM
 #simplest possible neural network. It has 1 layer, and that layer has 1 neuron, and the input shape to it is just 1 value
 model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
