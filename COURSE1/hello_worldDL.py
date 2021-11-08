030c8707f1e8ca2a9d625f2a369e3c54e30f4dec
aryan619348 <as2pilla@uwaterloo.ca>
2021-11-08 22:53:17 +0700

    Created Better_Fashion_Mnist

--- COURSE1/hello_worldDL.py
+++ COURSE1/hello_worldDL.py
@@ -1,3 +1,4 @@
+#%%
 import tensorflow as tf
 import numpy as np
 from tensorflow import keras
@@ -5,7 +6,7 @@ from tensorflow import keras
 #simplest possible neural network. It has 1 layer, and that layer has 1 neuron, and the input shape to it is just 1 value
 model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
-
+#%%
 #providing data
 xs = np.array([-1.0,  0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
@@ -15,3 +16,8 @@ model.fit(xs, ys, epochs=500)
 
 #check prediction for x=10
 print(model.predict([10.0]))
+#%%
+
+
+#%%
+
